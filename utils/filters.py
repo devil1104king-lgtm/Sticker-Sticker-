b1227a902a7fffe30b571d05bfaacd51fec7d0b9
devil1104king-lgtm <devil1104king@gmail.com>
2026-06-29 20:15:11 +0530

    Update filters.py

--- utils/filters.py
+++ utils/filters.py
@@ -1,1 +1,14 @@
+from telegram.ext import filters
+from config import Config
 
+class AdminFilter(filters.BaseFilter):
+    """Filter that allows only the owner."""
+
+    def __init__(self):
+        self.owner_id = Config.OWNER_ID
+
+    async def __call__(self, update, context):
+        user = update.effective_user
+        return user and user.id == self.owner_id
+
+admin_filter = AdminFilter()
